b338f343f63e52441105ea251a1cfb9503269e9c
lucasFilppe <lucasfilipe2323@icloud.com>
2023-02-28 08:14:44 -0300

    criando primeireiro estado

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - yarn add styled-components: instalação
 - yarn add @types/styled-components -D
 - [styled-components](https://www.alura.com.br/artigos/react-componentes-com-styled-components)
+
+### `api`
+
+- useEffect : recebe uma função como primeiro parametro e um segundo parametro é um array de dependencias. No primeiro parametro adicionamos a função que queremos que execute, ja no segundo parametro adicionamos quando é que essa função sera executada
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,12 +1,17 @@
+import { useState } from "react";
 import logoImg from "../../assets/logo.svg";
 import { Container, Content } from "./styles";
 
 export function Header() {
+
+  const [countTransaction, setCountTransaction] = useState(0);
+
   return (
       <Container>
         <Content>
           <img src={logoImg} alt="personal-finances" />
-          <button>Nova transação</button>
+          <h1>Transações feitas: {countTransaction}</h1>
+          <button onClick={()=> setCountTransaction(countTransaction + 1)}>Nova transação</button>
         </Content>
       </Container>
   );
