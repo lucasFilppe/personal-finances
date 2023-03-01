06a5534d882bca6d5f82de7a35bd3a4c5d016b0f
lucasFilppe <lucasfilipe2323@icloud.com>
2023-03-01 10:22:29 -0300

    atualizando redme

--- README.md
+++ README.md
@@ -8,4 +8,6 @@
 
 - useEffect : recebe uma função como primeiro parametro e um segundo parametro é um array de dependencias. No primeiro parametro adicionamos a função que queremos que execute, ja no segundo parametro adicionamos quando é que essa função sera executada
 - fech: [documentação](https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch);
-
+- mirage: O Mirage JS é uma biblioteca de simulação de API que permite criar, testar e compartilhar um aplicativo JavaScript funcional completo sem depender de nenhum serviço de back-end.[miragejs.com](https://miragejs.com/
+- axios: 
+Cliente HTTP baseado em promessas para o navegador e Node.js [axios.com](https://axios-http.com/ptbr/docs/intro)
--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createServer } from "miragejs";
+import App from "./App";
+
+createServer({
+  routes() {
+    this.namespace = "api";
+
+    this.get('/transactions', () => {
+      return[
+        {
+          id: 1,
+          title: 'Transaction 1',
+          amount: 400,
+          category: 'Compras',
+          creatAt: new Date()
+        }
+      ]
+    })
+  }
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
